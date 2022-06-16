ac48240a595b436ddc2376e1c1e972cb1de6dd6f
ravelinejunior <junior.raveline@gmail.com>
2022-06-16 00:31:12 -0300

    Add Address UI

--- app/src/main/java/br/com/raveline/mystorephi/presentation/fragment/AddAddressFragment.kt
+++ app/src/main/java/br/com/raveline/mystorephi/presentation/fragment/AddAddressFragment.kt
@@ -1,17 +1,18 @@
 package br.com.raveline.mystorephi.presentation.fragment
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import br.com.raveline.mystorephi.R
+import br.com.raveline.mystorephi.databinding.FragmentAddAddressBinding
 import br.com.raveline.mystorephi.databinding.FragmentAddressBinding
 
-class AddressFragment : Fragment() {
+class AddAddressFragment : Fragment() {
 
-    private var addressBinding: FragmentAddressBinding? = null
+    private var addressBinding: FragmentAddAddressBinding? = null
     private val binding get() = addressBinding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,21 +24,17 @@ class AddressFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        addressBinding = FragmentAddressBinding.inflate(inflater, container, false)
+        addressBinding = FragmentAddAddressBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.toolbarAddressFragment.setNavigationOnClickListener {
+        binding.toolbarAddAddressFragment.setNavigationOnClickListener {
             findNavController().popBackStack()
         }
 
-        binding.buttonAddressFragmentAddAddress.setOnClickListener {
-            findNavController().navigate(R.id.action_addressFragment_to_addAddressFragment)
-        }
     }
 
     override fun onDestroy() {
